4804cf29b4c9d095d6155d1a8f380ba5958f7af1
robrib1820 <rib21006@byui.edu>
2023-01-07 21:34:42 -0300

    prep1 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lName}, {name} {lName}");
     }
 }
