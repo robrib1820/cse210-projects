5850332c094970b83aa70192983e180615ab29f7
robrib1820 <rib21006@byui.edu>
2023-01-11 01:17:47 -0300

    prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        PromptUserName();
+        int favNumber = PromptUserNumber();
+        Console.Write(SquareNumber(favNumber));
+        Console.ReadLine();
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string nAme = Console.ReadLine();
+        return nAme;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int favNumber = int.Parse(Console.Read());
+        return favNumber;
+    }
+    static int SquareNumber(int number)
+    {
+        int sqNumber = number * number;
+        return sqNumber;
     }
 }
