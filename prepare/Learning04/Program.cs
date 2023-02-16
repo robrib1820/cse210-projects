2e7b6dbb507082bda79a2c8fad44b9b97a875ec7
Robson Ribeiro <rib21006@byui.edu>
2023-02-16 02:03:18 -0300

    learning04 completed

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samuel Bennter", "Multiplication");
+        string assigs = assignment.GetSummary();
+        Console.WriteLine(assigs);
+        Console.WriteLine("-----------------------------");
+        MathAssignment math = new MathAssignment("Section 7.5", "Problems 3-12", "Robson Ribeiro", "Fractions");
+        string mathAssigs = math.GetSummary();
+        Console.WriteLine(mathAssigs);
+        string mathProblems = math.GetHomewokList();
+        Console.WriteLine(mathProblems);
+        WritingAssignment writing = new WritingAssignment("Karlyn Eliza", "European History", "The Causes of World War II by Mary Waters");
+       string write = writing.GetSummary();
+       string writingInfo = writing.GetWritingInformation();
+       Console.WriteLine("--------------------------------");
+       Console.WriteLine(write);
+       Console.WriteLine(writingInfo);
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,11 @@
+using System;
+public class WritingAssignment : Assignment {
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic){
+        _title = title;
+    }
+    public string GetWritingInformation() {
+        return _title;
+    }
+}
